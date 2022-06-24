605d4d868bf310601f6de60d314a64b71f62183c
cleberpires32 <cleberpires269@gmail.com>
2022-06-24 14:44:54 -0300

    component tecnico-delete criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecn
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import { NgxMaskModule } from 'ngx-mask';
 import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
+import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecnico-delete.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +38,8 @@ import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecn
     TecnicoListComponent,
     LoginComponent,
     TecnicoCreateComponent,
-    TecnicoUpdateComponent
+    TecnicoUpdateComponent,
+    TecnicoDeleteComponent
   ],
   imports: [
     BrowserModule,
