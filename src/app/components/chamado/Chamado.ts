0f5738fd18bf47e313502e4e6da711afc607b2dc
cleberpires32 <cleberpires269@gmail.com>
2022-06-26 12:20:20 -0300

    criação domain Chamado.ts

---
+++ src/app/components/chamado/Chamado.ts
@@ -0,0 +1,14 @@
+export interface Chamado
+{
+  id: any
+  dataAbertura : string
+  dataFechamento: string
+  status: string
+  prioridade: any
+  titulo: string
+  observacoes: string
+  cliente: any
+  nomeCliente: string
+  tecnico: any
+  nomeTecnico: string
+}
