b41a3fc800a9a149fc1fa9ae5c6d9607870abf32
cleberpires32 <cleberpires269@gmail.com>
2022-06-28 20:42:31 -0300

    validação de campos implementados

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,3 +1,4 @@
+import { Validators, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChamadoCreateComponent implements OnInit {
 
+  titulo: FormControl = new FormControl(null, Validators.required);
+  prioridade: FormControl = new FormControl(null, Validators.required);
+  status: FormControl = new FormControl(null, Validators.required);
+  tecnico: FormControl = new FormControl(null, Validators.required);
+  cliente: FormControl = new FormControl(null, Validators.required);
+  descricao: FormControl = new FormControl(null, Validators.required);
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  validaCampos(): boolean {
+    if (this.titulo.valid && this.prioridade.valid && this.status.valid && this.tecnico.valid && this.cliente.valid && this.descricao.valid) {
+      return true
+    } else { return false }
+  }
 }
