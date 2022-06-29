901a7c266250b93899b76952d4ae29f9ff03cbfa
cleberpires32 <cleberpires269@gmail.com>
2022-06-29 11:02:01 -0300

    implementação HTML, CSS e TS do CHAMADO-READ

--- src/app/components/chamado/chamado-read/chamado-read.component.ts
+++ src/app/components/chamado/chamado-read/chamado-read.component.ts
@@ -1,4 +1,8 @@
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { ChamadoService } from './../../../services/chamado.service';
 import { Component, OnInit } from '@angular/core';
+import { Chamado } from '../Chamado';
 
 @Component({
   selector: 'app-chamado-read',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChamadoReadComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private chamadoService: ChamadoService,
+    private toastrService: ToastrService,
+    private actvRouter: ActivatedRoute) { }
+
+  chamado: Chamado = {
+    id: '',
+    dataAbertura: '',
+    dataFechamento: '',
+    status: '',
+    prioridade: '',
+    titulo: '',
+    observacoes: '',
+    cliente: '',
+    nomeCliente: '',
+    tecnico: '',
+    nomeTecnico: ''
+  };
 
   ngOnInit(): void {
+    this.chamado.id = this.actvRouter.snapshot.paramMap.get('id');
+    this.findById();
+  }
+
+  findById(): void {
+    this.chamadoService.findById(this.chamado.id).subscribe(response => {
+      this.chamado = response;
+    }, ex => { this.toastrService.warning('Chamado não encontrado') })
   }
 
 }
