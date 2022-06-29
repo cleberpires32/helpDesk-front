22cc5f7c9296de99a0bdd0eb3a51791263e940ea
cleberpires32 <cleberpires269@gmail.com>
2022-06-28 22:22:27 -0300

    HTML CSS e TS do CHAMADO-UPDATE criado.

--- src/app/components/chamado/chamado-update/chamado-update.component.ts
+++ src/app/components/chamado/chamado-update/chamado-update.component.ts
@@ -1,4 +1,13 @@
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { ChamadoService } from './../../../services/chamado.service';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { TecnicoService } from 'src/app/services/tecnico.service';
+import { Tecnico } from './../../tecnico/tecnico';
+import { Validators, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../../cliente/cliente';
+import { Chamado } from '../Chamado';
 
 @Component({
   selector: 'app-chamado-update',
@@ -7,9 +16,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChamadoUpdateComponent implements OnInit {
 
-  constructor() { }
+  tit: FormControl = new FormControl(null, Validators.required);
+  pri: FormControl = new FormControl(null, Validators.required);
+  sta: FormControl = new FormControl(null, Validators.required);
+  tec: FormControl = new FormControl(null, Validators.required);
+  cli: FormControl = new FormControl(null, Validators.required);
+  obs: FormControl = new FormControl(null, Validators.required);
+
+  constructor(
+    private tecnicoService: TecnicoService,
+    private clienteService: ClienteService,
+    private chamadoService: ChamadoService,
+    private toastrService: ToastrService,
+    private router: Router) { }
+
+  chamado: Chamado = {
+    id: '',
+    dataAbertura: '',
+    dataFechamento: '',
+    status: '',
+    prioridade: '',
+    titulo: '',
+    observacoes: '',
+    cliente: '',
+    nomeCliente: '',
+    tecnico: '',
+    nomeTecnico: ''
+  };
+
+  listaTecnicos: Tecnico[] = [];
+  listaClientes: Cliente[] = [];
 
   ngOnInit(): void {
+    this.findTecnicos()
+    this.findClientes()
+    console.log(this.validaCampos);
+
+  }
+
+  create(): void {
+    this.chamadoService.create(this.chamado).subscribe(response =>{
+      console.log(response);
+      this.toastrService.success('Chamado salvo com sucesso','Novo Chamado');
+      this.router.navigate(['chamados']);
+    }, ex => {
+      this.toastrService.error(ex.error.error);
+    })
+  }
+
+  validaCampos(): boolean {
+    if (this.tit.valid && this.pri.valid && this.sta.valid && this.tec.valid && this.cli.valid && this.obs.valid) {
+      return true
+    } else { return false }
+  }
+
+  findTecnicos(): void {
+    this.tecnicoService.findAll().subscribe(response => {
+      this.listaTecnicos = response;
+    })
+  }
+
+  findClientes(): void {
+    this.clienteService.findAll().subscribe(response => {
+      this.listaClientes = response;
+    })
   }
 
 }
