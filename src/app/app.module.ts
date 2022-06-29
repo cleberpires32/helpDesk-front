2cbbb8aa4126ec5f16a61eefedd810a462a0fe60
cleberpires32 <cleberpires269@gmail.com>
2022-06-29 10:41:22 -0300

    criaçãdo do componnent CHAMADO-READ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,6 +36,7 @@ import { ClienteDeleteComponent } from './components/cliente/cliente-delete/clie
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
+import { ChamadoReadComponent } from './components/chamado/chamado-read/chamado-read.component';
 
 @NgModule({
   declarations: [
@@ -54,7 +55,8 @@ import { ChamadoUpdateComponent } from './components/chamado/chamado-update/cham
     ClienteDeleteComponent,
     ChamadoListComponent,
     ChamadoCreateComponent,
-    ChamadoUpdateComponent
+    ChamadoUpdateComponent,
+    ChamadoReadComponent
   ],
   imports: [
     BrowserModule,
