80db85b5fb557b632badca3c588ef42a300e4997
cleberpires32 <cleberpires269@gmail.com>
2022-07-06 10:04:17 -0300

    criado service ItensEstoqueService

---
+++ src/app/services/itens-estoque.service.ts
@@ -0,0 +1,18 @@
+import { API_CONFIG } from './../config/api.config';
+import { Observable } from 'rxjs';
+import { ItensEstoque } from './../components/itens/ItensEstoque';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItensEstoqueService {
+
+  constructor(private http: HttpClient) { }
+
+  create(itensEstoque: ItensEstoque): Observable<ItensEstoque>{
+    return this.http.post<ItensEstoque>(`${API_CONFIG.baseUrl}/itensPedidos`, itensEstoque);
+  }
+
+}
