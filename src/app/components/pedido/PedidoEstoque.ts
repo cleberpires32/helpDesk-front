2287c7949ed05fea4f2b5af37a66760c17f85cec
cleberpires32 <cleberpires269@gmail.com>
2022-07-17 00:18:13 -0300

    implementação do Service Pedido de estoque

---
+++ src/app/components/pedido/PedidoEstoque.ts
@@ -0,0 +1,4 @@
+export interface PedidoEstoque {
+  chamado_id: string;
+  itensEstoque_id: string[]
+}
--- src/app/services/pedido-estoque.service.ts
+++ src/app/services/pedido-estoque.service.ts
@@ -1,3 +1,7 @@
+import { API_CONFIG } from './../config/api.config';
+import { Observable } from 'rxjs';
+import { PedidoEstoque } from './../components/pedido/PedidoEstoque';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +9,9 @@ import { Injectable } from '@angular/core';
 })
 export class PedidoEstoqueService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  delete(pedidoEstoque: PedidoEstoque): Observable<PedidoEstoque> {
+    return this.http.delete<PedidoEstoque>(`${API_CONFIG.baseUrl}/pedidosEstoques`);
+  }
 }
