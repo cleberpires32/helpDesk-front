7f3c6e3b0dc362328a2b38b061dea49a5e5fc4e0
cleberpires32 <cleberpires269@gmail.com>
2022-09-06 13:34:20 -0300

    implementação da aba pendencias do component ANDAMENTO

--- src/app/components/status/andamento/andamento.component.ts
+++ src/app/components/status/andamento/andamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-andamento',
@@ -10,6 +11,36 @@ export class AndamentoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  dataNow = new Date('2021-04-23T10:00:00.000');
+  txtpendencia = '';
+  listPendencia: string[] = [];
+
+  addPendencia(){
+  if( this.notNull() ){
+    this.listPendencia.push(this.txtpendencia);
+    this.txtpendencia = '';
+    }
+  }
+
+
+  removePendencia(event: Event, pen: string) {
+    if(event){
+        this.listPendencia.forEach((p, index)=>{
+          if(pen == p){
+            this.listPendencia.splice(index,1)
+          }
+        })
+     }
+  }
+
+  notNull(){
+    if(this.txtpendencia!="" && !(this.txtpendencia.match(/^\s+$/))){
+        return true;
+    }else{
+        return false;
+        }
   }
 
 }
