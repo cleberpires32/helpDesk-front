b68e509d4b3b37f6fbf2b3eeed0234f2e1fa90da
cleberpires32 <cleberpires269@gmail.com>
2022-09-22 22:14:50 -0300

    Apontamento base URL para heroku

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG ={
-  baseUrl : 'http://localhost:8080'
-  //baseUrl : 'https://helpdesk-backend-api.herokuapp.com'
+  //baseUrl : 'http://localhost:8080'
+  baseUrl : 'https://helpdesk-backend-api.herokuapp.com'
 }
